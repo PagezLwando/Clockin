251b266f683c56ae450b710e58d86fddb7e33056
lwando <lwandonodume@gmail.com>
2022-12-11 16:21:06 +0200

    Added a new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './shared/modules/materials/materials.module';
+import { TopComponent } from './shared/layout/top/top.component';
+import { MiddleComponent } from './shared/layout/middle/middle.component';
+import { BottomComponent } from './shared/layout/bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    MiddleComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
