2bcd63aba1323cbe13335525a245e30d162ead07
lwando <lwandonodume@gmail.com>
2022-12-17 12:49:10 +0200

    Added form register

--- src/app/modules/components/register/register.component.ts
+++ src/app/modules/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    cellNumber: new FormControl(''),
+    password: new FormControl(''),
+    cpassword: new FormControl(''),
+  })
+
 
   ngOnInit(): void {
+    console.log('init called');
+  }
+
+  onSubmit(): void {
+    console.log('Submitted');
   }
 
 }
